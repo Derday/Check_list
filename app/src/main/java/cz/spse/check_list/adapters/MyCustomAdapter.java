bf0ea033a6e254d6bb38a0ea1a61e4b8f6b06edf
Derday <dada.hofm@gmail.com>
2023-06-14 00:51:33 +0200

    Cosmetics

--- app/src/main/java/cz/spse/check_list/adapters/MyCustomAdapter.java
+++ app/src/main/java/cz/spse/check_list/adapters/MyCustomAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -18,6 +19,8 @@ import cz.spse.check_list.model.Note;
 public class MyCustomAdapter extends BaseAdapter {
     private Context context;
     private List<Note> notes;
+    private ColorStateList green, orange;
+
     public MyCustomAdapter(Context context, List<Note> notes) {
         this.context = context;
         this.notes = notes;
@@ -34,18 +37,26 @@ public class MyCustomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View convertView, ViewGroup parent) {
-
+        green = ColorStateList.valueOf(Color.rgb(85, 255, 0));
+        orange = ColorStateList.valueOf(Color.rgb(255, 128, 0));
 
         LayoutInflater layInflater = (LayoutInflater)
                 context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = layInflater.inflate(R.layout.note_item, null);
         TextView tv_name = convertView.findViewById(R.id.name_item);
         TextView tv_desc = convertView.findViewById(R.id.desc_item);
+        ImageView state = convertView.findViewById(R.id.imageView);
 
         Note note = notes.get(i);
 
         tv_name.setText(note.getName());
-        tv_desc.setText(note.getDescription()+"");
+        tv_desc.setText(note.getDescription());
+
+        if (note.isFinished() == 0){
+            state.setBackgroundColor(Color.rgb(255, 128, 0));
+        } else {
+            state.setBackgroundColor(Color.rgb(85, 255, 0));
+        }
 
 
 
